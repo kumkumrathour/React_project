13be97a1413ea800e66653b16da5297ff9ed056d
kumkum <kumkumrathour00@gmail.com>
2022-09-21 18:05:31 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import ExpenseItem from './components/ExpenseItem';
-
+import React from 'react'
 import './App.css';
+import Expenses from './components/Expenses/Expenses'
 
-function App() {
+const App = () => {
       const expenses = [
         {
+          id:'e1',
           title:'car insurance', 
           amount:46.32, 
-          date:new Date(2054,3,13)
+          date:new Date(2020,3,13)
       },
         {
+          id:'e2',
           title:'bike insurance',
            amount:792.32,
-            date:new Date(2076,3,13)
+            date:new Date(2022,5,3)
           },
         {
+          id:'e3',
           title:'airoplane insurance',
            amount:934.32, 
-           date:new Date(2021,3,13)
+           date:new Date(2021,7,23)
           },
         {
+          id:'e4',
           title:'train insurance', 
           amount:32.32,
-           date:new Date(2032,3,13)
+           date:new Date(2019,9,15)
           }
       ]
 
@@ -30,18 +34,9 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-  <a
-    className="App-link"
-      target="_blank"
-         rel="noopener noreferrer">
-          <h2> Welcome to React with kumkum</h2>
-          <ExpenseItem/>  
-          <ExpenseItem/>  
-          <ExpenseItem/>  
-            </a>
-        </header>
+    <div>
+          <h1> Welcome to React </h1>
+          <Expenses items={expenses}/>
     </div>
   );
 }
